f8743187f988c26ee181683254a525830c7da1a3
natali-alkayed <nataly2000alkayed@gmail.com>
2023-06-10 14:46:51 +0000

    linked list done without wiht bord

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,56 @@
-'use strict';
+ 'use strict';
 
-// Require our linked list implementation
-const LinkedList = require('../index');
+const LinkedList = require("../lib/LinkedList");
 
-describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+describe("LinkedList", () => {
+  let myList;
+
+  beforeEach(() => {
+    myList = new LinkedList();
+  });
+
+  it("should instantiate an empty linked list", () => {
+    expect(myList.head).toBeNull();
+  });
+
+  it("should properly insert into the linked list", () => {
+    myList.insert("a");
+    expect(myList.head.value).toEqual("a");
+  });
+
+  it("should make the head property point to the first node", () => {
+    myList.insert("a");
+    myList.insert("b");
+    expect(myList.head.value).toEqual("b");
+  });
+
+  it("should properly insert multiple nodes into the linked list", () => {
+    myList.insert("a");
+    myList.insert("b");
+    myList.insert("c");
+    expect(myList.head.value).toEqual("c");
+    expect(myList.head.next.value).toEqual("b");
+    expect(myList.head.next.next.value).toEqual("a");
+  });
+
+  it("should return true when finding a value within the linked list that exists", () => {
+    myList.insert("a");
+    myList.insert("b");
+    myList.insert("c");
+    expect(myList.includes("b")).toBe(true);
+  });
+
+  it("should return false when searching for a value in the linked list that does not exist", () => {
+    myList.insert("a");
+    myList.insert("b");
+    myList.insert("c");
+    expect(myList.includes("d")).toBe(false);
+  });
+
+  it("should properly return a collection of all values in the linked list", () => {
+    myList.insert("a");
+    myList.insert("b");
+    myList.insert("c");
+    expect(myList.toString()).toEqual("{ c } -> { b } -> { a } -> NULL");
   });
 });
